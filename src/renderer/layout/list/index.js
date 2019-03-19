c1a09d444836a837924c2db94b19b9a5848f595b
zenoven <zenoven@gmail.com>
2019-03-19 20:54:09 +0800

    feat: style list with table

--- src/renderer/layout/list/index.js
+++ src/renderer/layout/list/index.js
@@ -1,23 +1,45 @@
 import { connect } from 'dva';
+import { Table, Progress, Icon } from 'antd';
 import { remote } from 'electron';
 import is from 'electron-is';
 import styles from './index.less';
 import { formatTask } from 'utils';
 
-const List = ({ task, ...props }) => {
-  let { list } = task;
+const getColumns = (props) => ([
+  {
+    dataIndex: 'name',
+    title: '名称',
+  },
+  {
+    dataIndex: 'progress',
+    title: '进度',
+    render(text) {
+      text = parseFloat(text);
+      let done = text === 100;
+      let type = done ? 'circle' : 'line';
+      return done ? <Icon type='check-circle' className={styles.done} /> : <Progress type={type} percent={text} showInfo={!done} />
+    }
+  },
+  {
+    dataIndex: 'downloadSpeed',
+    title: '速度',
+  },
+]);
+
+const List = (props) => {
+  let {
+    task: {
+      list
+    }
+  } = props;
   return (
     <div className={styles.list}>
-      <ul>
-        {
-          list.map(formatTask).map(({gid, name, progress, ...item}, index) => {
-            console.log('item props', Object.keys(item));
-            console.log('name:', name);
-            console.log('percent:', progress);
-            return <li key={gid}>{name}-{progress}</li>;
-          })
-        }
-      </ul>
+      <Table
+        rowKey='gid'
+        columns={getColumns(props)}
+        dataSource={list.map(formatTask)}
+        pagination={false}
+      />
     </div>
   )
 }
